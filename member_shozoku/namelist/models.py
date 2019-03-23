574f394ca33ed853a90568888a7481f8a2d6a39c
ライヴライブ 開発 <develop@livlib.co.jp>
2019-03-23 16:59:55 +0900

    Commit on Mar/23/2019

--- member_shozoku/namelist/models.py
+++ member_shozoku/namelist/models.py
@@ -1,20 +1,16 @@
 from django.db import models
-
-# Create your models here.
 from django.urls import reverse
 
 class Member(models.Model):
-    name = models.CharField(max_length=100)
-    year_of_birth = models.IntegerField()
+    name = models.CharField(max_length=30)
+    year_of_birth = models.IntegerField(null=True)
 
     syozoku = models.ForeignKey('Syozoku', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        """String for representing the Model object."""
         return self.name
 
     def get_absolute_url(self):
-        """Returns the url to access a detail record for this book."""
         return reverse('member-detail', args=[str(self.id)])
 
 class Syozoku(models.Model):
@@ -25,9 +21,7 @@ class Syozoku(models.Model):
         ordering = ['name']
 
     def get_absolute_url(self):
-        """Returns the url to access a particular author instance."""
         return reverse('syozoku-detail', args=[str(self.id)])
 
     def __str__(self):
-        """String for representing the Model object."""
         return f'{self.name}'
